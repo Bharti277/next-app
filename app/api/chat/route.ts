37285e0bb76309c3c518501b0e9625a2390ce1a7
Bharti277 <bhartideependra277@gmail.com>
2025-08-16 09:39:42 +0530

    mini chat app

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,34 @@
+import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export async function POST(request: Request) {
+export async function POST(request) {
   try {
-    const { messages } = await request.json();
-    await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages,
+    const { message } = await request.json();
+
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: message }],
     });
-    return new Response("Message sent successfully", { status: 200 });
-  } catch (error) {}
+
+    // const fakeResponse = `🤖 MockBot says: You typed "${message}"`;
+    return NextResponse.json({
+      response: completion.choices[0].message.content,
+      // response: fakeResponse,
+    });
+  } catch (error) {
+    console.log(error, "error in chat route");
+
+    return NextResponse.json(
+      {
+        error: error.message || "Failed to process the request",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [streaming, setStreaming] = useState("");
+  const [streamRes, setStreamRes] = useState("");
+
+  const handleChat = async () => {
+    setLoading(true);
+    setResponse("");
+
+    try {
+      const res = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await res.json();
+      console.log(data, "data");
+
+      setResponse(data.response);
+      setLoading(false);
+    } catch (error) {
+      setResponse("Error" + error.message);
+    }
+  };
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Hello</h1>
+    <div>
+      <textarea
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Enter your awesome message"
+        rows={4}
+      ></textarea>
+      <div>
+        <button onClick={handleChat}>{loading ? "Loading..." : "Chat"}</button>
+      </div>
+      <div>{response}</div>
     </div>
   );
 }
